e044aac70fba4310ea0e39ae651033399cfed00e
monokitai <chaosknete@gmail.com>
2017-06-27 18:56:48 +0200

    v01

---
+++ cmake-build-debug/CMakeFiles/hammer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hammer.dir/main.cpp.o"
+  "hammer.pdb"
+  "hammer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hammer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+#include <thread>
+#include <tins/tins.h>
+
+int main() {
+
+    Tins::NetworkInterface iface = Tins::NetworkInterface::default_interface();
+
+    Tins::RawPDU *message = new Tins::RawPDU("Monster Energy <3");
+    Tins::TCP *tcp = new Tins::TCP();
+    Tins::IP *ip = new Tins::IP();
+    Tins::SNAP *snap = new Tins::SNAP();
+    Tins::Dot11Data *dot11 = new Tins::Dot11Data();
+
+    dot11->inner_pdu(snap);
+    snap->inner_pdu(ip);
+    ip->inner_pdu(tcp);
+    tcp->inner_pdu(message);
+
+    Tins::PacketSender sender;
+
+    dot11->send(sender, iface);
+    
+    return 0;
+}
+
+
